b4a80d9dee0c12a7024ff03a7b4881370dad1c23
ElodieSayavong <elodie.sayavong.dev@gmail.com>
2022-02-23 13:30:36 +0100

    [front] create and navbar button styled component

--- front/src/component/button/index.js
+++ front/src/component/button/index.js
@@ -1,7 +1,22 @@
-const Button = () => {
+import { styled } from "@mui/material";
+
+const StyledButton = props => {
+    const Button = styled("div")({
+        backgroundColor: "white",
+        width: props.width,
+        height: "2rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "999px",
+        color: "#5F9EA0",
+        cursor: "pointer",
+        margin: "1rem",
+    });
+
     return (
-        <div>button</div>
+        <Button>{props.children}</Button>
     );
 };
 
-export default Button;
+export default StyledButton;
--- front/src/component/navbar/index.js
+++ front/src/component/navbar/index.js
@@ -1,19 +1,106 @@
+import { useState } from "react";
 // DEPENDENCIES IMPORTATIONS
-import { useNavigate } from "react-router-dom";
-// MUI IMPORTATIONS
 import {
     Box,
-    Button,
+    Toolbar,
+    IconButton,
+    Typography,
+    Menu,
+    Avatar,
+    Tooltip,
+    MenuItem,
 } from "@mui/material";
+import { styled } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+// COMPONENTS IMPORTATIONS
+import StyledButton from "../button";
+
+const StyledAppBar = styled("div")({
+    backgroundColor: "#5F9EA0",
+    display: "flex",
+    justifyContent: "end",
+});
 
 const Navbar = () => {
+    const [anchorElUser, setAnchorElUser] = useState(null);
     const navigate = useNavigate();
 
+    const handleOpenUserMenu = event => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleRedirect = path => {
+        navigate(path);
+    };
+
+    const handleLogout = () => {
+        // add logout request
+    };
+
+    const LoggedNavbar = () => {
+        return (
+            <Toolbar>
+                <Box>
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        sx={{ mt: '45px' }}
+                        anchorOrigin={{
+                            vertical: "top",
+                            horizontal: "right",
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: "top",
+                            horizontal: "right",
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        <MenuItem>
+                            <Typography onClick={() => handleRedirect("/dashboard")}>Dashboard</Typography>
+                        </MenuItem>
+                        <MenuItem>
+                            <Typography onClick={handleLogout}>Logout</Typography>
+                        </MenuItem>
+                    </Menu>
+                </Box>
+            </Toolbar>
+        );
+    };
+
+    const NoLoggedNavbar = () => {
+        return (
+            <Toolbar>
+                <StyledButton
+                    variant="contained"
+                    width="8rem"
+                    onClick={() => handleRedirect("/login")}>
+                    Login
+                </StyledButton>
+                <StyledButton
+                    variant="outlined"
+                    width="8rem"
+                    onClick={() => handleRedirect("/register")}>
+                    Logout
+                </StyledButton>
+            </Toolbar>
+        );
+    };
+
     return (
-        <Box>
-            <Button variant="contained" onClick={() => navigate("/login")}>Se connecter</Button>
-            <Button variant="contained" onClick={() => navigate("/register")}>Créer un compte</Button>
-        </Box>
+        <StyledAppBar position="static">
+            { LoggedNavbar()}
+        </StyledAppBar>
     );
 };
 
